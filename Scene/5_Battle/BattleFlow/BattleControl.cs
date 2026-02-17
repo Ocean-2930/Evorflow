ece4ae24943cc4ec2c88c24ba1842206c0716496
DESKTOP-VQF3J20\New User <oceanblue1590@gmail.com>
2026-02-18 01:08:19 +0900

    Feat: Battle Control 초안

---
+++ Scene/5_Battle/BattleFlow/BattleControl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleControl : SceneSingleton<BattleControl>
+{
+    public override string className => "BattleControl";
+
+    private IEnumerator _nextCoroutine;
+    public bool endBattle = false;
+
+    void Start()
+    {
+        _nextCoroutine = null;
+        endBattle = false;
+        StartCoroutine(MainLoop(new BattleFlow()));
+    }
+
+    public void NextCoroutine(IEnumerator co)
+    {
+        if (_nextCoroutine == null)
+        {
+            _nextCoroutine = co;
+        }
+    }
+
+    private IEnumerator MainLoop(BattleFlow bflow)
+    {
+        while (true)
+        {
+            _nextCoroutine = null;
+            yield return new WaitUntil(() => (_nextCoroutine != null || endBattle));
+
+            if (_nextCoroutine != null) { yield return StartCoroutine(_nextCoroutine); }
+
+            if (endBattle) { break; }
+        }
+
+        yield return null;
+    }
+}
